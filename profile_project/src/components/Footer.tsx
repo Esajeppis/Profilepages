972692ce472693fbdac83e053f8cc1dc02d34a23
Esa Heikkila <esa.heikkila@student.lab.fi>
2024-12-04 17:27:23 +0200

    contactinfo

--- profile_project/src/components/Footer.tsx
+++ profile_project/src/components/Footer.tsx
@@ -4,8 +4,29 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-dark text-white text-center py-3 mt-auto">
+    <footer className="bg-dark text-white text-center py-4 mt-auto">
       <div className="container">
+        <div className="contact-info">
+          <p>Email: <a href="mailto:espekki@hotmail.com" className="text-white">espekki@hotmail.com</a></p>
+          <p>
+            <a 
+              href="www.linkedin.com/in/esa-heikkilä-b3b58a225" 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className="text-white"
+            >
+              LinkedIn
+            </a> | 
+            <a 
+              href="https://github.com/Esajeppis?tab=repositories" 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className="text-white"
+            >
+              GitHub
+            </a>
+          </p>
+        </div>
         <p>&copy; {currentYear} Esa Heikkilä. All Rights Reserved.</p>
       </div>
     </footer>
